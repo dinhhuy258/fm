82f5763f337c6ffa79395e120594b3864f56c746
Huy Duong <huy.duongdinh@gmail.com>
2021-09-18 22:47:17 +0700

    Refactor

--- pkg/mode/default_mode.go
+++ pkg/mode/default_mode.go
@@ -2,48 +2,6 @@ package mode
 
 import "github.com/dinhhuy258/fm/pkg/message"
 
-func createDeleteMode() *Mode {
-	return &Mode{
-		Name: "delete",
-		KeyBindings: &KeyBindings{
-			OnKeys: map[string]*Action{
-				"d": {
-					Help: "delete",
-					Messages: []message.Message{
-						{
-							Func: message.DeleteCurrent,
-						},
-					},
-				},
-				"s": {
-					Help: "delete selections",
-					Messages: []message.Message{
-						{
-							Func: message.DeleteSelections,
-						},
-					},
-				},
-				"esc": {
-					Help: "cancel",
-					Messages: []message.Message{
-						{
-							Func: message.PopMode,
-						},
-					},
-				},
-				"q": {
-					Help: "quit",
-					Messages: []message.Message{
-						{
-							Func: message.Quit,
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
 func createDefaultMode() *Mode {
 	return &Mode{
 		Name: "default",
