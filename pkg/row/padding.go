2efcfb91f98af67f5b8fafd61d29dc7a6a9cfbda
Huy Duong <huy.duongdinh@gmail.com>
2021-10-11 20:16:30 +0700

    Truncate long path in help panel mark load

--- pkg/row/padding.go
+++ pkg/row/padding.go
@@ -8,7 +8,10 @@ import (
 // Right right-pads the string with pad up to len runes
 func Right(str string, length int, pad string) string {
 	count := length - utf8.RuneCountInString(str)
+
 	if count <= 0 {
+		str = str[(-count)+2:]
+		str = "..." + str
 		count = 1
 	}
 
