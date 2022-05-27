842a104f2455629c2e8412184b074970db93a637
Huy Duong <huy.duongdinh@gmail.com>
2022-05-27 21:50:49 +0700

    Fix input view

--- pkg/gui/view/input_view.go
+++ pkg/gui/view/input_view.go
@@ -6,54 +6,54 @@ import (
 )
 
 type InputView struct {
-	v      *View
+	*View
 	prompt string
 }
 
 func newInputView(g *gocui.Gui, v *gocui.View) *InputView {
 	iv := &InputView{
-		v: newView(g, v),
+		View: newView(g, v),
 	}
 
 	iv.prompt = "> "
-	iv.v.v.Title = " Input "
+	iv.v.Title = " Input "
 
 	return iv
 }
 
 func (iv *InputView) SetInputBuffer(input string) {
-	iv.v.v.TextArea.TypeString(iv.prompt + input)
-	iv.v.v.RenderTextArea()
+	iv.v.TextArea.TypeString(iv.prompt + input)
+	iv.v.RenderTextArea()
 
-	_, _ = iv.v.g.SetCurrentView(iv.v.v.Name())
-	iv.v.SetViewOnTop()
+	_, _ = iv.g.SetCurrentView(iv.v.Name())
+	iv.SetViewOnTop()
 }
 
 func (iv *InputView) GetInputBuffer() string {
-	return iv.v.v.BufferLines()[0][len(iv.prompt):]
+	return iv.v.BufferLines()[0][len(iv.prompt):]
 }
 
 func (iv *InputView) UpdateInputBufferFromKey(key key.Key) {
-	iv.v.g.Update(func(g *gocui.Gui) error {
+	iv.g.Update(func(g *gocui.Gui) error {
 		switch k := key.(type) {
 		case rune:
-			iv.v.v.TextArea.TypeRune(key.(rune))
+			iv.v.TextArea.TypeRune(key.(rune))
 		case gocui.Key:
 			switch {
 			case k == gocui.KeyBackspace || k == gocui.KeyBackspace2:
-				iv.v.v.TextArea.DeleteChar()
+				iv.v.TextArea.DeleteChar()
 			case k == gocui.KeyArrowLeft:
-				x, _ := iv.v.v.Cursor()
+				x, _ := iv.v.Cursor()
 
 				if x > len(iv.prompt) {
-					iv.v.v.TextArea.MoveCursorLeft()
+					iv.v.TextArea.MoveCursorLeft()
 				}
 			case k == gocui.KeyArrowRight:
-				iv.v.v.TextArea.MoveCursorRight()
+				iv.v.TextArea.MoveCursorRight()
 			}
 		}
 
-		iv.v.v.RenderTextArea()
+		iv.v.RenderTextArea()
 
 		return nil
 	})
