abae45fa2adb5f4e85fbcdc30c53d8dbda6827ec
Huy Duong <huy.duongdinh@gmail.com>
2022-06-02 22:38:21 +0700

    Fix lint

--- pkg/fs/file_entry.go
+++ pkg/fs/file_entry.go
@@ -1,12 +1,15 @@
 package fs
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/djherbis/times"
 )
+
+var errFileNotFound = errors.New("file not found")
 
 // IEntry represents a file or directory.
 type IEntry interface {
@@ -113,87 +116,102 @@ func LoadEntries(path string,
 	entries := make([]IEntry, 0, len(names))
 
 	for _, name := range names {
-		absolutePath := filepath.Join(path, name)
-
-		lstat, err := os.Lstat(absolutePath)
+		entry, err := loadEntry(path, name, showHidden)
 		if err != nil {
-			if os.IsNotExist(err) {
+			if errors.Is(err, errFileNotFound) {
 				continue
 			}
 
 			return nil, err
 		}
 
-		if !showHidden && isHidden(name) {
-			continue
-		}
-
-		var ct time.Time
-
-		ts := times.Get(lstat)
-		if ts.HasChangeTime() {
-			ct = ts.ChangeTime()
-		} else {
-			ct = lstat.ModTime()
-		}
-
-		isDir := lstat.IsDir()
-		size := lstat.Size()
-		permissions := lstat.Mode().String()[1:]
-		isSymlink := (lstat.Mode() & os.ModeSymlink) != 0
-		if isSymlink {
-			linkTarget, err := os.Readlink(absolutePath)
-			if err != nil {
-				return nil, err
-			}
-
-			linkTargetLstat, err := os.Lstat(linkTarget)
-			if err != nil {
-				if os.IsNotExist(err) {
-					continue
-				}
-
-				return nil, err
-			}
-
-			isDir = linkTargetLstat.IsDir()
-		}
-
-		ext := filepath.Ext(absolutePath)
-		if ext != "" {
-			ext = ext[1:]
-		}
-
-		if isDir {
-			entries = append(entries, &Directory{
-				&Entry{
-					name:        name,
-					path:        absolutePath,
-					size:        size,
-					permissions: permissions,
-					ext:         ext,
-					changeTime:  ct,
-					isSymlink:   isSymlink,
-				},
-			})
-		} else {
-			entries = append(entries, &File{
-				&Entry{
-					name:        name,
-					path:        absolutePath,
-					size:        size,
-					permissions: permissions,
-					ext:         ext,
-					changeTime:  ct,
-					isSymlink:   isSymlink,
-				},
-			})
-		}
+		entries = append(entries, entry)
 	}
 
 	getEntrySort(sortType(sortAlgorithm)).sort(entries, sortReverse, sortIgnoreCase, sortIgnoreDiacritics)
 
 	return entries, nil
+}
+
+// loadEntry loads the entry of the given file path and file name.
+func loadEntry(path, name string, showHidden bool) (IEntry, error) {
+	fpath := filepath.Join(path, name)
+
+	lstat, err := os.Lstat(fpath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, errFileNotFound
+		}
+
+		return nil, err
+	}
+
+	if !showHidden && isHidden(name) {
+		return nil, errFileNotFound
+	}
+
+	var ct time.Time
+
+	ts := times.Get(lstat)
+	if ts.HasChangeTime() {
+		ct = ts.ChangeTime()
+	} else {
+		ct = lstat.ModTime()
+	}
+
+	isDir := lstat.IsDir()
+	size := lstat.Size()
+	permissions := lstat.Mode().String()[1:]
+
+	isSymlink := (lstat.Mode() & os.ModeSymlink) != 0
+	if isSymlink {
+		linkTarget, err := os.Readlink(fpath)
+		if err != nil {
+			return nil, err
+		}
+
+		linkTargetLstat, err := os.Lstat(linkTarget)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return nil, errFileNotFound
+			}
+
+			return nil, err
+		}
+
+		isDir = linkTargetLstat.IsDir()
+	}
+
+	var ext string
+	if ext = filepath.Ext(fpath); ext != "" {
+		ext = ext[1:]
+	}
+
+	if isDir {
+		return &Directory{
+			&Entry{
+				name:        name,
+				path:        fpath,
+				size:        size,
+				permissions: permissions,
+				ext:         ext,
+				changeTime:  ct,
+				isSymlink:   isSymlink,
+			},
+		}, nil
+	}
+
+	return &File{
+		&Entry{
+			name:        name,
+			path:        fpath,
+			size:        size,
+			permissions: permissions,
+			ext:         ext,
+			changeTime:  ct,
+			isSymlink:   isSymlink,
+		},
+	}, nil
 }
 
 // isHidden returns true if the given name is a hidden file.
