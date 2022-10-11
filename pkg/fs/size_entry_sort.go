40cf021393c1656fcb4ad30ece9ab3d6a379725c
Huy Duong <huy.duongdinh@gmail.com>
2022-10-11 16:58:53 +0700

    Improve size entry sort algorithm

--- pkg/fs/size_entry_sort.go
+++ pkg/fs/size_entry_sort.go
@@ -10,11 +10,32 @@ type sizeEntrySort struct{}
 // sort by file size
 func (sizeEntrySort) sort(entries []IEntry, reverse bool, ignoreCase bool, ignoreDiacritics bool) {
 	sort.Slice(entries, func(i, j int) bool {
-		s := entries[i].GetSize() > entries[j].GetSize()
-		if reverse {
-			s = !s
+		entry1 := entries[i]
+		entry2 := entries[j]
+
+		if !entry1.IsDirectory() && !entry2.IsDirectory() {
+			// if both entries are files
+			s := entry1.GetSize() > entry2.GetSize()
+			if reverse {
+				s = !s
+			}
+
+			return s
 		}
 
-		return s
+		if entry1.IsDirectory() && entry2.IsDirectory() {
+			// If 2 entries are both directory or file, then sort by their name
+			name1 := normalize(entry1.GetName(), ignoreCase, ignoreDiacritics)
+			name2 := normalize(entry2.GetName(), ignoreCase, ignoreDiacritics)
+
+			return name1 < name2
+		}
+
+		if entry1.IsDirectory() {
+			return true
+		}
+
+		// Second entry is directory
+		return false
 	})
 }
